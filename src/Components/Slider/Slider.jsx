b27f3610fa5b8966d0b89e4dd852283881fd90d8
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-07-08 16:08:18 +0500

    first

---
+++ src/Components/Slider/Slider.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+import styles from "./Slider.module.css";
+import img1 from "../../Assets/slider/slide1.webp";
+import img2 from "../../Assets/slider/slide2.webp";
+function Slider() {
+  return (
+    <div className="slider">
+      <Splide
+        options={{
+          rewind: true,
+          width: "100%",
+          perPage: 1,
+          perMove: 1,
+          arrows: false,
+          pagination: true,
+        }}
+      >
+        <SplideSlide>
+          <div className={styles.slideBox}>
+            <div className={styles.leftSlide}>
+              <h1>Win $105,095 in Lottery</h1>
+              <div className={styles.timer}>
+                <div className={styles.time}>
+                  <h1>0</h1>
+                  <p>h</p>
+                </div>
+                <div className={styles.time}>
+                  <h1>00</h1>
+                  <p>m</p>
+                </div>
+                <div className={styles.time}>
+                  <h1>00</h1>
+                  <p>s</p>
+                </div>
+              </div>
+
+              <button className={styles.mainBtn}>
+                Play Now <FontAwesomeIcon icon={faArrowRight} />
+              </button>
+            </div>
+            <div className={styles.rightSlide2}>
+              <img src={img1} alt="" />
+            </div>
+          </div>
+        </SplideSlide>
+        <SplideSlide>
+          <div className={styles.slideBox}>
+            <div className={styles.leftSlide}>
+              <h4>Perpetual Futures</h4>
+              <h2>UP TO 100× LEVERAGE</h2>
+              <button className={styles.mainBtn}>
+                Trade Now <FontAwesomeIcon icon={faArrowRight} />
+              </button>
+            </div>
+            <div className={styles.rightSlide1}>
+              <img src={img2} alt="" />
+            </div>
+          </div>
+        </SplideSlide>
+      </Splide>
+    </div>
+  );
+}
+
+export default Slider;
--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Slider from "../../Components/Slider/Slider";
 import styles from "./styles/Home.module.css";
 function Home() {
-  return <div className={styles.home}>Home</div>;
+  return (
+    <div className={styles.home}>
+      <Slider />
+    </div>
+  );
 }
 
 export default Home;
